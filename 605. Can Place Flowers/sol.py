ecb18f3cb2d394b21d6c10e6c2f52f808a30685d
Somesh Raj <someshraj78569@gmail.com>
2024-06-09 13:11:41 +0530

    Update sol.py

--- 605. Can Place Flowers/sol.py
+++ 605. Can Place Flowers/sol.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        count = i = 0
+        while i < len(flowerbed):
+            if flowerbed[i] == 0:
+                if i == len(flowerbed) - 1 or flowerbed[i + 1] == 0:
+                    count += 1
+                    i += 2
+                else:
+                    i += 1
+            else:
+                i += 2
+        return count >= n
