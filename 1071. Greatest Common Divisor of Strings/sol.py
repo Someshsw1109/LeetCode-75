ecb3be9b637bb9719214a4f465aea9f99b95f667
Somesh Raj <someshraj78569@gmail.com>
2024-06-09 13:08:08 +0530

    Create sol.py

---
+++ 1071. Greatest Common Divisor of Strings/sol.py
@@ -0,0 +1,9 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+        def gcd(x, y):
+            while y:
+                x, y = y, x % y
+            return x
+        return str1[:gcd(len(str1), len(str2))]
